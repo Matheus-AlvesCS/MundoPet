99c7ba1b50853e4ca360ebbc7047338d30e2dc2f
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2026-02-17 11:18:07 -0300

    Stylizing and animating modal

--- src/main.js
+++ src/main.js
@@ -4,4 +4,22 @@ const btnClose = document.getElementById("closeModal")
 
 btnOpen.onclick = () => modal.showModal()
 
-btnClose.onclick = () => modal.close()
+btnClose.onclick = () => {
+  modal.classList.add("disappear")
+
+  modal.addEventListener(
+    "animationend",
+    () => {
+      modal.classList.remove("disappear")
+
+      modal.close()
+    },
+    { once: true },
+  )
+}
+
+modal.addEventListener("cancel", (event) => {
+  event.preventDefault()
+
+  btnClose.click()
+})
